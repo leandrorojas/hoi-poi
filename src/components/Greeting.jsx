22b35bebc0a65e4492cb117c4dd2d439ec9d3d04
leandrorojas <leandrorojas@gmail.com>
2026-04-10 17:52:19 -0300

    Revert "fix: add prop-types validation to Greeting component (#13)" (#14)
    This reverts commit 58e8cc2b7fd82b0379c9c9114839af4e417919de.

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,11 +1,5 @@
-import PropTypes from "prop-types";
-
 function Greeting({ name = "World" }) {
   return <h2>Hello, {name}!</h2>;
 }
-
-Greeting.propTypes = {
-  name: PropTypes.string,
-};
 
 export default Greeting;
